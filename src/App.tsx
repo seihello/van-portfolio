47679aef6c12988e4db79408acf2dfb04ade5cc3
Seisuke Yamada <momoiropuchoman@gmail.com>
2023-07-16 23:05:34 -0700

    Separate WorkExperience

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import classes from './App.module.scss'
 import AboutMe from './AboutMe'
 import ProjectList from './ProjectList'
 import SkillList from './SkillList'
+import WorkExperienceList from './WorkExperienceList'
 
 function App() {
 
@@ -11,31 +12,10 @@ function App() {
       <AboutMe />
       <ProjectList />
       <SkillList />
-
+      <WorkExperienceList />
         
 
-        <div className="section">
-            <h2>Work Experience</h2>
-            <hr/>
-            <div className="experience-first">
-                <div className="org-name">
-                    <a className="link" href="https://www.muratec.jp/" target="_blank"><h3>Muratec Machinery</h3></a>
-                </div>
-                <h3>Apr. 2017 - Aug. 2022</h3>
-            </div>
-            <div className="work-item">
-                <div className="work-description">
-                    <p className="role">Software Engineer</p>
-                    <ul>
-                        <li>Developed a large-scale software for controlling machines which work in semiconductor factories</li>
-                        <li>Designed the architecture, created C++ codes, conducted testing</li>
-                        <li>Managed external development teams and reviewed their deliverables</li>
-                        <li>Communicated with our clients and local workers to arrange schedules and report problems</li>
-                    </ul>
-                </div>
-                <img className="work-image" src="./semicon.jpg" />
-            </div>
-        </div>
+        
 
         <div className="section">
             <h2>Education</h2>
