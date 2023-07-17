632e1b4455cc5772a1fd31f27d165b10f499758c
Seisuke Yamada <momoiropuchoman@gmail.com>
2023-07-17 01:00:28 -0700

    Make 3 second timer

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,6 +1,19 @@
 import React from 'react'
+import { useEffect, useState, useRef } from 'react'
 
 export default function Header() {
+  const intervalRef = useRef(0);
+
+  useEffect(() => {
+    intervalRef.current = setInterval(() => {
+      console.log('3 seconds passed');
+    }, 3000);
+
+    return () => {
+      clearInterval(intervalRef.current);
+    };
+  }, []);
+
   return (
     <header>
       <img id="station-image" src="./station.jpg" />
