6ae917ed10c79fe36aa8642c17a1b6278a0cb258
Seisuke Yamada <momoiropuchoman@gmail.com>
2023-07-16 23:08:43 -0700

    Separate Resume

---
+++ src/Resume.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Resume() {
+  return (
+    <div className="section">
+      <h2>Résumé</h2>
+      <hr />
+      <div className="line">
+        <img id="pdf-icon" src="./pdf.png" />
+        <a className="resume" href="./resume/Seisuke_Yamada_Resume.pdf"><h3>Résumé (English)</h3></a>
+      </div>
+    </div>
+  )
+}
