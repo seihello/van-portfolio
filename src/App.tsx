3f420d7ad52bf98cea972b5751d7f2c10eeb115c
Seisuke Yamada <momoiropuchoman@gmail.com>
2023-07-16 23:02:13 -0700

    Separate Skill

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import classes from './App.module.scss'
 import AboutMe from './AboutMe'
 import ProjectList from './ProjectList'
+import SkillList from './SkillList'
 
 function App() {
 
@@ -9,111 +10,9 @@ function App() {
     <body>
       <AboutMe />
       <ProjectList />
+      <SkillList />
+
         
-
-        <div className="section">
-            <h2>Skill</h2>
-            <hr/>
-            <h3>Computer Language</h3>
-            <div className="skills">
-                <div className="skill">
-                    <p className="skill-name">C++</p>
-                    <div className="skill-rate-box"><div className="skill-rate rate100"></div></div>
-                </div>
-                <div className="skill">
-                    <p className="skill-name">C</p>
-                    <div className="skill-rate-box"><div className="skill-rate rate100"></div></div>
-                </div>
-                <div className="skill">
-                    <p className="skill-name">Python</p>
-                    <div className="skill-rate-box"><div className="skill-rate rate80"></div></div>
-                </div>
-                <div className="skill">
-                    <p className="skill-name">Java</p>
-                    <div className="skill-rate-box"><div className="skill-rate rate80"></div></div>
-                </div>
-                <div className="skill">
-                    <p className="skill-name">HTML/CSS</p>
-                    <div className="skill-rate-box"><div className="skill-rate rate60"></div></div>
-                </div>
-                <div className="skill">
-                    <p className="skill-name">JavaScript</p>
-                    <div className="skill-rate-box"><div className="skill-rate rate60"></div></div>
-                </div>
-                <div className="skill">
-                    <p className="skill-name">PHP</p>
-                    <div className="skill-rate-box"><div className="skill-rate rate20"></div></div>
-                </div>
-                <div className="skill">
-                    <p className="skill-name">Swift</p>
-                    <div className="skill-rate-box"><div className="skill-rate rate40"></div></div>
-                </div>
-                <div className="skill">
-                    <p className="skill-name">SQL</p>
-                    <div className="skill-rate-box"><div className="skill-rate rate80"></div></div>
-                </div>
-            </div>
-
-            <h3>Tool</h3>
-            <div className="skills">
-                <div className="skill">
-                    <p className="skill-name">Git</p>
-                    <div className="skill-rate-box"><div className="skill-rate rate100"></div></div>
-                </div>
-                <div className="skill">
-                    <p className="skill-name">GitHub</p>
-                    <div className="skill-rate-box"><div className="skill-rate rate80"></div></div>
-                </div>
-                <div className="skill">
-                    <p className="skill-name">Enterprise Architecture</p>
-                    <div className="skill-rate-box"><div className="skill-rate rate100"></div></div>
-                </div>
-                <div className="skill">
-                    <p className="skill-name">Redmine</p>
-                    <div className="skill-rate-box"><div className="skill-rate rate80"></div></div>
-                </div>
-            </div>
-
-            <h3>OS</h3>
-            <div className="skills">
-                <div className="skill">
-                    <p className="skill-name">Windows</p>
-                    <div className="skill-rate-box"><div className="skill-rate rate100"></div></div>
-                </div>
-                <div className="skill">
-                    <p className="skill-name">macOS</p>
-                    <div className="skill-rate-box"><div className="skill-rate rate80"></div></div>
-                </div>
-                <div className="skill">
-                    <p className="skill-name">Linux</p>
-                    <div className="skill-rate-box"><div className="skill-rate rate40"></div></div>
-                </div>
-            </div>
-
-            <h3>Other</h3>
-            <div className="skills">
-                <div className="skill">
-                    <p className="skill-name">OOP</p>
-                    <div className="skill-rate-box"><div className="skill-rate rate100"></div></div>
-                </div>
-                <div className="skill">
-                    <p className="skill-name">UML</p>
-                    <div className="skill-rate-box"><div className="skill-rate rate100"></div></div>
-                </div>
-                <div className="skill">
-                    <p className="skill-name">DevOps</p>
-                    <div className="skill-rate-box"><div className="skill-rate rate80"></div></div>
-                </div>
-                <div className="skill">
-                    <p className="skill-name">Agile Development</p>
-                    <div className="skill-rate-box"><div className="skill-rate rate60"></div></div>
-                </div>
-                <div className="skill">
-                    <p className="skill-name">SEMI Standards</p>
-                    <div className="skill-rate-box"><div className="skill-rate rate80"></div></div>
-                </div>
-            </div>
-        </div>
 
         <div className="section">
             <h2>Work Experience</h2>
