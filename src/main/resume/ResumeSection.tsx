9d520c378ee4f3ca6f85e464c9055a47530a0a8c
Seisuke Yamada <momoiropuchoman@gmail.com>
2023-07-25 23:26:21 -0700

    Separate the style for Resume

--- src/main/resume/ResumeSection.tsx
+++ src/main/resume/ResumeSection.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import styles from './resume.module.scss'
 
 export default function ResumeSection() {
   return (
@@ -6,7 +7,7 @@ export default function ResumeSection() {
       <h2>Résumé</h2>
       <hr />
       <div className="line">
-        <img id="pdf-icon" src="./pdf.png" />
+        <img className={styles.pdf_icon} src="./pdf.png" />
         <a className="resume" href="./resume/Seisuke_Yamada_Resume.pdf"><h3>Résumé (English)</h3></a>
       </div>
     </div>
