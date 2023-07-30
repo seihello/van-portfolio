683194ad048920868a5db6cdddee4296564047d6
Seisuke Yamada <momoiropuchoman@gmail.com>
2023-07-30 14:11:03 -0700

    Use framer motion to move elements

--- src/header/Arrival.tsx
+++ src/header/Arrival.tsx
@@ -1,6 +1,6 @@
 import styles from './header.module.scss'
 import { forwardRef } from 'react';
-
+import { motion, useAnimation, AnimationControls } from "framer-motion";
 
 export enum AnimationState {
   None,
@@ -14,15 +14,16 @@ type ArrivalProps = {
   destination: string;
   eta: string;
   index: number;
+  control: AnimationControls;
 };
 
 
 export default function Arrival(props: ArrivalProps) {
   return (
-    <div className={styles.arrival} style={{top: `${props.index*33}%`}}>
+    <motion.div animate={props.control} transition={{ duration: 1 }} className={styles.arrival} style={{top: `${props.index*33}%`}}>
       <img className={styles.train_icon} src="./train.png" />
       <div className={styles.destination}>{props.destination}</div>
       <div className={styles.eta}>{props.eta}</div>
-    </div>
+    </motion.div>
   )
 };
--- src/header/Header.tsx
+++ src/header/Header.tsx
@@ -2,6 +2,7 @@ import { useLayoutEffect, useState, useEffect, useRef } from 'react'
 import Arrival from './Arrival'
 import BoardClock from './BoardClock'
 import styles from './header.module.scss'
+import { useAnimation } from "framer-motion";
 
 const introductions = [
   "Hello, I'm Seisuke!",
@@ -12,13 +13,24 @@ const introductions = [
 export default function Header() {
   const [eta, setEta] = useState([getETA(0), getETA(1), getETA(2)])
 
+  const controls = useAnimation();
+
+  const startAnimation = async () => {
+    // 要素Aのxのアニメーションを開始
+    await controls.start({ x: "100%" });
+  };
+
+  useEffect(() => {
+    startAnimation();
+  }, []); 
+
   return (
     <header className={styles.header}>
       <div className={styles.board}>
         <div className={styles.schedule}>
-          {<Arrival key={0} destination={introductions[0]} eta={eta[0]} index={0} />}
-          {<Arrival key={1} destination={introductions[1]} eta={eta[1]} index={1} />}
-          {<Arrival key={2} destination={introductions[2]} eta={eta[2]} index={2} />}
+          {<Arrival control={controls} key={0} destination={introductions[0]} eta={eta[0]} index={0} />}
+          {<Arrival control={controls} key={1} destination={introductions[1]} eta={eta[1]} index={1} />}
+          {<Arrival control={controls} key={2} destination={introductions[2]} eta={eta[2]} index={2} />}
         </div>
         <div className={styles.info}>
           <div className={styles.announce}>
