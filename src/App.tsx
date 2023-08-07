3ba57a4aa4a752c5a3dd2941b19122e494d107af
Seisuke Yamada <momoiropuchoman@gmail.com>
2023-08-07 02:23:52 -0700

    Separate the pages

--- src/App.tsx
+++ src/App.tsx
@@ -7,20 +7,25 @@ import WorkExperienceSection from './main/experience/WorkExperienceSection'
 import EducationSection from './main/experience/EducationSection'
 import ResumeSection from './main/resume/ResumeSection'
 import ContactSection from './main/contact/ContactSection'
+import { Routes, Route, BrowserRouter } from 'react-router-dom'
 
 function App() {
-  
+
   return (
     <>
       <Header />
       <body>
-        <AboutMeSection />
-        <ProjectSection />
-        <SkillSection />
-        <WorkExperienceSection />
-        <EducationSection />
-        <ResumeSection />
-        <ContactSection />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<AboutMeSection />} />
+            <Route path="/projects" element={<ProjectSection />} />
+            <Route path="/skill" element={<SkillSection />} />
+            <Route path="/work-experience" element={<WorkExperienceSection />} />
+            <Route path="/education" element={<EducationSection />} />
+            <Route path="/resume" element={<ResumeSection />} />
+            <Route path="/contact" element={<ContactSection />} />
+          </Routes>
+        </BrowserRouter>
       </body>
       <Footer />
     </>
