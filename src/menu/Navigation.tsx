13d02315ff10f6d6de9513cec9d0f40b7d2c6d53
Seisuke Yamada <momoiropuchoman@gmail.com>
2023-08-13 00:31:39 -0700

    Close the mobile menu

--- src/menu/Navigation.tsx
+++ src/menu/Navigation.tsx
@@ -4,8 +4,16 @@ import { mobileMenuContext } from '../App';
 
 export default function Navigation() {
   const mobileMenuRef = useContext(mobileMenuContext)
+  const onMobileMenuCloseClicked = () => {
+    if(mobileMenuRef.current) {
+      mobileMenuRef.current.style.display = "none";
+    }
+  }
+
+
   return (
     <div className={styles.nav} ref={mobileMenuRef}>
+      <i className={`fa-solid fa-xmark ${styles.mobile_menu_close}`} onClick={onMobileMenuCloseClicked}></i>
       Mobile Menu
     </div>
   );
