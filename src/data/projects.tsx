c47e022cc25423b488fe0ed3da535e53df55e72a
Seisuke Yamada <momoiropuchoman@gmail.com>
2023-09-05 01:59:51 -0700

    refactor: Load project data from ts object

---
+++ src/data/projects.tsx
@@ -0,0 +1,60 @@
+const projects = [
+  {
+    name: "Reversi",
+    description: "This Reversi game allows you to play against a computer opponent, engage in two-player matches, and even observe computer vs. computer battles.",
+    demoLink: "",
+    gitLink: "https://github.com/seihello/Reversi",
+    image: "reversi.png",
+    skills: ["Python", "TKinter"]
+  },
+  {
+    name: "English Learning",
+    description: "An English vocabulary learning web application that features word-meaning-example sets, category-based filtering, and user-friendly word bookmarking for focused learning.",
+    demoLink: "https://english.seisuke.info/",
+    gitLink: "https://github.com/seihello/EverydayEnglish-on-Browser",
+    image: "everydayenglish-browser.png",
+    skills: ["HTML", "CSS(Sass)", "JavaScript", "JQuery"]
+  },
+  {
+    name: "English Learning(iOS)",
+    description: "An English vocabulary learning web application for iOS",
+    demoLink: "",
+    gitLink: "https://github.com/seihello/EverydayEnglish",
+    image: "everydayenglish.png",
+    skills: ["Python", "Kivy"]
+  },
+  {
+    name: "Strategy Game",
+    description: "A Two-player chess-like strategy game where the objective is to infiltrate your opponent's territory for victory. Users can implement their own coding strategies for gameplay.",
+    demoLink: "",
+    gitLink: "https://github.com/seihello/AnimalMaster",
+    image: "animalmaster.png",
+    skills: ["Python", "TKinter"]
+  },
+  {
+    name: "Comment Bot",
+    description: "An app for automatically posting relevant comments during live streams. Users can schedule comments to be posted at specified intervals.",
+    demoLink: "",
+    gitLink: "https://github.com/seihello/LIVIT-Bot",
+    image: "chatbot.png",
+    skills: ["Python", "Selenium"]
+  },
+  {
+    name: "Weather App",
+    description: "A web application that displays weather information. It provides current weather, a 5-day forecast, and 3-hourly forecasts. Users can not only check the weather at their current location but also bookmark their favorite cities.",
+    demoLink: "https://weather.seisuke.info/",
+    gitLink: "https://github.com/seihello/weather-app",
+    image: "weatherapp.png",
+    skills: ["HTML", "CSS(Sass)", "JavaScript"]
+  },
+  {
+    name: "CSS Gallery",
+    description: "A web page showcasing the designed CSS modules.",
+    demoLink: "https://css-gallery.seisuke.info/",
+    gitLink: "https://github.com/seihello/css-gallery",
+    image: "cssgallery.png",
+    skills: ["HTML", "CSS(Sass)", "JavaScript"]
+  }
+]
+
+export default projects;
--- src/subpages/project/ProjectPage.tsx
+++ src/subpages/project/ProjectPage.tsx
@@ -1,29 +1,11 @@
 import Project from './Project'
 import styles from './project.module.scss'
 import commonStyles from '../../common.module.scss'
-import { useState, useEffect } from 'react';
+import projects from '../../data/projects'
 
 export default function ProjectPage() {
 
-  const [projects, setProjects] = useState([]);
-
-  useEffect(() => {
-    const jsonFile = "/data/projects.json";
-
-    fetch(jsonFile)
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        setProjects(data);
-      })
-      .catch((error) => {
-        console.error('Error:', error);
-      });
-  }, []);
-
   return (
-
     <>
       <div className={commonStyles.section}>
         <h2>Project</h2>
