b3b42e3b9db486826e1f2b7a119b78c62da0d70f
Seisuke Yamada <momoiropuchoman@gmail.com>
2023-09-11 02:03:07 -0700

    style: Update the profile

--- src/home/HomePage.tsx
+++ src/home/HomePage.tsx
@@ -8,7 +8,7 @@ export default function HomePage() {
         <div className={styles.profile}>
           <img className={styles.profile_image} src="/img/profile.jpg" />
           <div className={styles.profile_text}>
-            Hello, I'm Seisuke! After graduating with a degree in Computer Science, I have dedicated over 5 years to software development. My expertise spans a wide range, from working on large-scale embedded applications to web development. Along the way, I realized that I have a passion for creating efficient, maintainable, and user-friendly systems. My strengths are this passion and strong responsibility. I aim to carry my past experiences forward, while continuing to grow.
+            Hello, I'm Seisuke, a full stack software developer. I have dedicated over 5 years to software development after graduating with a degree in Computer Science. My expertise spans a wide range, from working on large-scale embedded applications to web development. Along the way, I realized that I have a passion for creating efficient, maintainable, and user-friendly systems. My strengths are this passion and strong responsibility. I aim to carry my past experiences forward, while continuing to grow.
           </div>
         </div>
       </div>
