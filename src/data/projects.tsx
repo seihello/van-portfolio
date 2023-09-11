d27be0ca8310830035d290d0a529d9bf5d91b86b
Seisuke Yamada <momoiropuchoman@gmail.com>
2023-09-11 02:28:54 -0700

    style: Change the order of the projects

--- src/data/projects.tsx
+++ src/data/projects.tsx
@@ -1,11 +1,19 @@
 const projects = [
   {
-    name: "Reversi",
-    description: "This Reversi game allows you to play against a computer opponent, engage in two-player matches, and even observe computer vs. computer battles.",
-    demoLink: "",
-    gitLink: "https://github.com/seihello/Reversi",
-    image: "reversi.png",
-    skills: ["Python", "TKinter"]
+    name: "Citation Generator",
+    description: "A web application that displays information related to article or paper citations, allowing users to customize the design and export it as an image.",
+    demoLink: "https://cite-so.vercel.app/",
+    gitLink: "",
+    image: "cite.png",
+    skills: ["HTML", "CSS", "Next.js"]
+  },
+  {
+    name: "Corporate Website",
+    description: "A responsive corporate website consisting of multiple pages. Features include news feeds, subscriptions, and stock price information display.",
+    demoLink: "https://planetventuresinc.com/index.php",
+    gitLink: "",
+    image: "planet.png",
+    skills: ["HTML", "CSS", "Sass", "JavaScript", "PHP", "Joomla!"]
   },
   {
     name: "English Learning",
@@ -24,52 +32,12 @@ const projects = [
     skills: ["Python", "Kivy"]
   },
   {
-    name: "Strategy Game",
-    description: "A Two-player chess-like strategy game where the objective is to infiltrate your opponent's territory for victory. Users can implement their own coding strategies for gameplay.",
-    demoLink: "",
-    gitLink: "https://github.com/seihello/AnimalMaster",
-    image: "animalmaster.png",
-    skills: ["Python", "TKinter"]
-  },
-  {
-    name: "Comment Bot",
-    description: "An app for automatically posting relevant comments during live streams. Users can schedule comments to be posted at specified intervals.",
-    demoLink: "",
-    gitLink: "https://github.com/seihello/LIVIT-Bot",
-    image: "chatbot.png",
-    skills: ["Python", "Selenium"]
-  },
-  {
-    name: "Chat Bot",
-    description: "A chatbot running on the Gravity social media that automatically talks to users by recognizing their voices and generating natural responses using ChatGPT.",
-    demoLink: "",
-    gitLink: "https://github.com/seihello/robovity",
-    image: "default.png",
-    skills: ["Python", "ChatGPT"]
-  },
-  {
     name: "Weather App",
     description: "A web application that displays weather information. It provides current weather, a 5-day forecast, and 3-hourly forecasts. Users can not only check the weather at their current location but also bookmark their favorite cities.",
     demoLink: "https://weather.seisuke.info/",
     gitLink: "https://github.com/seihello/weather-app",
     image: "weatherapp.png",
     skills: ["HTML", "CSS", "Sass", "JavaScript"]
-  },
-  {
-    name: "CSS Gallery",
-    description: "A web page showcasing the designed CSS modules.",
-    demoLink: "https://css-gallery.seisuke.info/",
-    gitLink: "https://github.com/seihello/css-gallery",
-    image: "cssgallery.png",
-    skills: ["HTML", "CSS", "Sass", "JavaScript"]
-  },
-  {
-    name: "Citation Generator",
-    description: "A web application that displays information related to article or paper citations, allowing users to customize the design and export it as an image.",
-    demoLink: "https://cite-so.vercel.app/",
-    gitLink: "",
-    image: "cite.png",
-    skills: ["HTML", "CSS", "Next.js"]
   },
   {
     name: "Transport Controller",
@@ -80,13 +48,45 @@ const projects = [
     skills: ["C", "C++", "Vue.js"]
   },
   {
-    name: "Corporate Website",
-    description: "A responsive corporate website consisting of multiple pages. Features include news feeds, subscriptions, and stock price information display.",
-    demoLink: "https://planetventuresinc.com/index.php",
-    gitLink: "",
-    image: "planet.png",
-    skills: ["HTML", "CSS", "Sass", "JavaScript", "PHP", "Joomla!"]
-  }
+    name: "Chat Bot",
+    description: "A chatbot running on the Gravity social media that automatically talks to users by recognizing their voices and generating natural responses using ChatGPT.",
+    demoLink: "",
+    gitLink: "https://github.com/seihello/robovity",
+    image: "default.png",
+    skills: ["Python", "ChatGPT"]
+  },
+  {
+    name: "Comment Bot",
+    description: "An app for automatically posting relevant comments during live streams. Users can schedule comments to be posted at specified intervals.",
+    demoLink: "",
+    gitLink: "https://github.com/seihello/LIVIT-Bot",
+    image: "chatbot.png",
+    skills: ["Python", "Selenium"]
+  },
+  {
+    name: "Strategy Game",
+    description: "A Two-player chess-like strategy game where the objective is to infiltrate your opponent's territory for victory. Users can implement their own coding strategies for gameplay.",
+    demoLink: "",
+    gitLink: "https://github.com/seihello/AnimalMaster",
+    image: "animalmaster.png",
+    skills: ["Python", "TKinter"]
+  },
+  {
+    name: "Reversi",
+    description: "This Reversi game allows you to play against a computer opponent, engage in two-player matches, and even observe computer vs. computer battles.",
+    demoLink: "",
+    gitLink: "https://github.com/seihello/Reversi",
+    image: "reversi.png",
+    skills: ["Python", "TKinter"]
+  },
+  {
+    name: "CSS Gallery",
+    description: "A web page showcasing the designed CSS modules.",
+    demoLink: "https://css-gallery.seisuke.info/",
+    gitLink: "https://github.com/seihello/css-gallery",
+    image: "cssgallery.png",
+    skills: ["HTML", "CSS", "Sass", "JavaScript"]
+  },
 ]
 
 export default projects;
