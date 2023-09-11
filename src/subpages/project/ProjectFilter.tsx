76f7331baeb4e21be87e1558df58322c6233b218
Seisuke Yamada <momoiropuchoman@gmail.com>
2023-09-10 20:05:19 -0700

    feat: Prevent a state where no filter options are selected

--- src/subpages/project/ProjectFilter.tsx
+++ src/subpages/project/ProjectFilter.tsx
@@ -24,15 +24,18 @@ export default function ProjectFilter() {
     const newSelectedSkills = new Set<string>(selectedSkills);
     if (filterSkill === "All") {
       if (selectedSkills.has(filterSkill)) {
-        newSelectedSkills.delete(filterSkill);
+        // Don't allow All to be unselected
+        return;
       } else {
         newSelectedSkills.clear();
         newSelectedSkills.add(filterSkill);
       }
-    }
-    else {
+    } else {
       if (selectedSkills.has(filterSkill)) {
         newSelectedSkills.delete(filterSkill);
+        if(newSelectedSkills.size === 0) {
+          newSelectedSkills.add("All");
+        }
       } else {
         newSelectedSkills.add(filterSkill);
         newSelectedSkills.delete("All");
