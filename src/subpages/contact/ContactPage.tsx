c9812c4e9541d2b99e85e204ca0f82f7f7abda1c
Seisuke Yamada <momoiropuchoman@gmail.com>
2023-09-26 00:40:41 -0700

    improvement: remove the resume section

--- src/subpages/contact/ContactPage.tsx
+++ src/subpages/contact/ContactPage.tsx
@@ -1,13 +1,11 @@
 import EmailSection from './EmailSection'
 import ContactSection from './ContactSection'
-import ResumeSection from './ResumeSection'
 
 export default function ContactPage() {
   return (
     <>
     <EmailSection />
       <ContactSection />
-      <ResumeSection/>
     </>
   )
 }
