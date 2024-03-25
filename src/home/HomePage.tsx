b96342bbc07221f97fb39d32e0eac0dbf07374f1
Seisuke Yamada <momoiropuchoman@gmail.com>
2024-03-25 00:31:50 -0700

    fix: modify the profile text

--- src/home/HomePage.tsx
+++ src/home/HomePage.tsx
@@ -8,7 +8,7 @@ export default function HomePage() {
         <div className={styles.profile}>
           <img className={styles.profile_image} src="/img/profile.jpg" />
           <div className={styles.profile_text}>
-            Hi, I'm Seisuke, a software engineer/Full-stack web developer with a Bachelor's degree in Computer Science and 5+ years of experience in software development. I have been dedicated to team goals with a passion for building efficient, maintainable, and user-friendly systems. This experience has provided me with the opportunity to acquire a wide range of skills and experience, spanning from large-scale embedded applications to web development.
+            Hi, I'm Seisuke, a software engineer/full-stack web developer with a Bachelor's degree in Computer Science and 5+ years of experience in software development. I have been dedicated to team goals with a passion for building efficient, maintainable, and user-friendly systems. This experience has provided me with the opportunity to acquire a wide range of skills and experience, spanning from large-scale embedded applications to web development.
 
             Throughout my ten-year journey in the field of software engineering, I have overcome numerous challenges in both technical and managerial capacities, enhancing my problem-solving skills along the way. With the experience I bring, coupled with my refined problem-solving abilities and steadfast perseverance when encountering difficulties, I'm eager to contribute significantly to your team!
           </div>
